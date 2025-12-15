7ff68ce9de9594466c5860982ec8815c41c12051
Bhavya <bhavya29shah10@gmail.com>
2025-12-15 14:08:38 +0530

    Made Pyhton code useful

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import os
 
+#Returns a list of all files and folders in the current working directory.
 print(os.listdir())
+
+#Returns the absolute path of the folder where your Python program is currently running.
+print(os.getcwd())
