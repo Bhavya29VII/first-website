8fef2dfa3890cae5ae4945bc91cf9b6c3382de7d
Bhavya <bhavya29shah10@gmail.com>
2025-12-15 14:14:32 +0530

    Changed Pyhton code

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import os
 
+# Returns a list of all files and folders in the current working directory.
 print(os.listdir())
+
+#os.get_blocking() is a low-level operating system function used to check whether a file descriptor is in blocking or non-blocking mode.
+print(os.get_blocking())
+
+#os.chdir() is used to change the current working directory of your Python program.
+print(os.chdir("/"))
